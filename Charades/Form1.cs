a8f759acffaef47fbaf560ce3a0089954cad3222
przemek207 <przemek.blokus@gmail.com>
2015-12-28 19:09:25 +0100

    Drawing into pictureBox and user interface maked

--- Charades/Form1.cs
+++ Charades/Form1.cs
@@ -12,9 +12,43 @@ namespace Charades
 {
     public partial class Form1 : Form
     {
+        private Graphics g;
+        private Point p = Point.Empty;
+        private Pen pioro;
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(400, 400);
+            g = Graphics.FromImage(pictureBox1.Image);
+            pioro = new Pen(Color.Black);
+
         }
-    }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                p = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                g.DrawLine(pioro, p, e.Location);
+                p = e.Location;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void clear()
+        {
+            g.Clear(Color.White);
+            pictureBox1.Refresh();
+        }
+    }     
 }
